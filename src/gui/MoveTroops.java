652ab9a40037f903d4542263d485d43c4dc87bd0
Gogolinolett <50708111+Gogolinolett@users.noreply.github.com>
2021-01-19 12:30:32 +0100

    .

--- src/gui/MoveTroops.java
+++ src/gui/MoveTroops.java
@@ -89,7 +89,7 @@ public class MoveTroops extends JFrame {
             JButton btn = (JButton) e.getSource();
             if(btn.equals(cButton)){
                 a2.setFarbeOwner(a1.getFarbeOwner());
-                if(troops < (int)spinner.getValue() && (int)spinner.getValue() < a1.getTroopCount()){
+                if(troops <= (int)spinner.getValue() && (int)spinner.getValue() < a1.getTroopCount()){
                     Main.getBoard().moveTroop(a1, a2, (int) spinner.getValue());
                     setVisible(false);
                 }
